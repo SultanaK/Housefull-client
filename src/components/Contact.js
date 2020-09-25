24cb191201c23ccbd7fc8618c60c533164e52bca
SultanaK <kr.shampa@yahoo.com>
2020-09-24 20:40:15 -0500

    fixed contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
         <div id="contact_container">
                     
             <h5>Let us know how to improve</h5>
-            <title>
+            
                   
-                <iframe title="contact-us" id ="contact" name="contact-us" src="https://docs.google.com/forms/d/e/1FAIpQLSc-uHUPImmRxscZCq_p8aGZW-ewN2Q4O2-Ad1hKPhJTB0ksZw/viewform?embedded=true" className="survey" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>  
-            </title>
+                
+            <iframe title ="contact-us" src="https://docs.google.com/forms/d/e/1FAIpQLSc-uHUPImmRxscZCq_p8aGZW-ewN2Q4O2-Ad1hKPhJTB0ksZw/viewform?embedded=true" width="640" height="1152" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     )
 }
