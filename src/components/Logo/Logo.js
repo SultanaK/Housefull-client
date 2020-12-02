fba59ac0efab108f61b1dd23867638a2931526a5
SultanaK <kr.shampa@yahoo.com>
2020-12-01 18:21:39 -0600

    fix HTML validation

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import houseLogo from '../../images/house-Black.png'
 import './Logo.css'
 export default function Logo() {
     return (
